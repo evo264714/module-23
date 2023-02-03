514bb68c98516ee93dbc4a5df3196762f096521f
evo264714 <mh264714@gmail.com>
2023-02-03 20:16:26 +0600

    first

---
+++ array.js
@@ -0,0 +1,19 @@
+const country = 'Bangladesh';
+const age = 52;
+const isIndependent = true;
+const student = {
+    id: 121,
+    class: 11,
+    name: 'Agun'
+};
+const friends = [13, 14, 11, 17, 21, 16];
+function add (num1, num2){
+    return num1 + num2;
+}
+
+console.log(typeof country);
+console.log(typeof age);
+console.log(typeof isIndependent);
+console.log(typeof student);
+console.log(typeof friends);
+console.log(typeof add);
