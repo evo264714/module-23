a18b68d52acd5d4b517bb3a07860d3fa6da07313
evo264714 <mh264714@gmail.com>
2023-02-04 14:07:10 +0600

    cart

--- simple-cart.js
+++ simple-cart.js
@@ -5,3 +5,15 @@ const shoppingCart = [
     {name: 'Belt', price: 600},
     {name: 'Perfume', price: 3200}
 ];
+
+function totalCost(products){
+    let sum = 0;
+    for(let i = 0; i < products.length; i++){
+        const product = products[i];
+        sum = sum + product.price;
+        // console.log(product);
+    }
+    return sum;
+}
+const expense = totalCost(shoppingCart);
+console.log('Total expense today: ', expense);
