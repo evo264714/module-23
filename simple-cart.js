0c825ea5a3787990e54c71975de44620387ae988
evo264714 <mh264714@gmail.com>
2023-02-04 13:51:49 +0600

    cheap

---
+++ simple-cart.js
@@ -0,0 +1,7 @@
+const shoppingCart = [
+    {name: 'Shoe', price: 1200},
+    {name: 'Shirt', price: 2200},
+    {name: 'Pant', price: 3700},
+    {name: 'Belt', price: 600},
+    {name: 'Perfume', price: 3200}
+];
